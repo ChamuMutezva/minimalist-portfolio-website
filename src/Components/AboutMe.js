6d0778563a2c7c94e8a15ab40a4e768afa49fa40
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-10-11 04:57:36 +0200

    feat: about component, tablet display

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,38 @@
+import AboutMeMobile from '../images/homepage/mobile/image-homepage-profile.jpg'
+import AboutMeTablet from '../images/homepage/tablet/image-homepage-profile.jpg'
+import AboutMeDesktop from '../images/homepage/desktop/image-homepage-profile.jpg'
+const AboutMe = () => {
+    return (
+        <div className="about__me">
+            <div className="about__img__container">
+                <picture>
+                    <source media="(min-width: 1080px)"
+                        srcSet={AboutMeDesktop} />
+                    <source media="(min-width: 620px)"
+                        srcSet={AboutMeTablet} />
+                    <img className="about__img" src={AboutMeMobile} alt="" />
+                </picture>
+            </div>
+            <div className="about__content">
+                <div className="about__content__holder">
+                    <h2 className="about__content__title">
+                        About me
+                    </h2>
+                    <p className="about__content__content">
+                        I’m a junior front-end developer looking for a new role in an exciting company.
+                        I focus on writing accessible HTML, using modern CSS practices and writing clean
+                        JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to
+                        whatever tools are required. I’m based in London, UK, but I’m happy working remotely
+                        and have experience in remote teams. When I’m not coding, you’ll find me outdoors.
+                        I love being out in nature whether that’s going for a walk, run or cycling. I’d love
+                        you to check out my work.
+                    </p>
+                    <a className="shared--btn" href="/">
+                        go to portfolio
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AboutMe
---
+++ src/Components/Hero.js
@@ -0,0 +1,32 @@
+import HeroMobile from '../images/homepage/mobile/image-homepage-hero.jpg'
+import HeroTablet from '../images/homepage/tablet/image-homepage-hero.jpg'
+import HeroDesktop from '../images/homepage/desktop/image-homepage-hero.jpg'
+import DownArrows from '../images/icons/down-arrows.svg'
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero__img__container">
+                <picture>
+                    <source media="(min-width: 1080px)"
+                        srcSet={HeroDesktop} />
+                    <source media="(min-width: 620px)"
+                        srcSet={HeroTablet} />
+                    <img className="hero__img" src={HeroMobile} alt="" />
+                </picture>
+            </div>
+            <div className="hero__content">
+                <h1 className="hero__content__title">
+                    Hey, I’m Alex Spencer and I love building beautiful websites
+                </h1>
+                <a className="btn" href="/">
+                    <div className="arrows__img__holder">
+                        <img className="arrows__img" src={DownArrows} alt="" />
+                    </div>
+                    <span className="btn__text">About me</span>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
