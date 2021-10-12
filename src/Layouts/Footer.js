e487e44201b6282533d69da6eb77c4809f995ae8
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-10-12 05:06:04 +0200

    feat: footer component - navigation created

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,52 @@
+import Logo from '../images/logo.svg'
+import Github from '../images/icons/github.svg'
+import Twitter from '../images/icons/twitter.svg'
+import Linkein from '../images/icons/linkedin.svg'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <div className="logo__container">
+                    <img className="footer__logo" src={Logo} alt="my frontend portfolio" />
+                </div>
+                <nav className="footer__nav">
+                    {/*Modal and navigation list items*/}
+                    <div className={`footer__nav__list`}>
+                        <ul className="footer__nav__list__items">
+                            <li className="footer__nav__list__item">
+                                <a className="footer__nav--item" href="/">Home</a>
+                            </li>
+                            <li className="footer__nav__list__item">
+                                <a className="footer__nav--item" href="/">Portfolio</a>
+                            </li>
+                            <li className="footer__nav__list__item">
+                                <a className="footer__nav--item" href="/">Contact me</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="social__list">
+                    <ul className="social__list__items">
+                        <li className="social__list__item">
+                            <a href="./" aria-label="preview my work on github">
+                                <img className="social__icons" src={Github} alt="" aria-hidden="true" />
+                            </a>
+                        </li>
+                        <li className="social__list__item">
+                            <a href="./" aria-label="lets connect on twitter">
+                                <img className="social__icons" src={Twitter} alt="" aria-hidden="true" />
+                            </a>
+                        </li>
+                        <li className="social__list__item">
+                            <a href="./" aria-label="lets connect on linkein">
+                                <img className="social__icons" src={Linkein} alt="" aria-hidden="true" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import HomeMain from "../Layouts/HomeMain"
 import Aside from "../Layouts/Aside"
+import Footer from "../Layouts/Footer"
 const HomePage = () => {
     return (
         <div>
             <HomeMain />
             <Aside />
+            <Footer />
         </div>
     )
 }
