ea0c807466987b142830812a544225e745e2faf5
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-10-20 19:29:04 +0200

    contact form desktop display

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -86,90 +86,96 @@ const ContactPage = () => {
 
     return (
         <main className="main">
-            <div className="contact">
+            <div className="contact__social__container">
                 <h1 className="contact__heading"> Get in Touch  </h1>
-                <p className="contact__content">
-                    I’d love to hear about what you’re working on and how I could help. I’m currently
-                    looking for a new role and am open to a wide range of opportunities. My preference
-                    would be to find a position in a company in London. But I’m also happy to hear about
-                    opportunites that don’t fit that description. I’m a hard-working and positive person
-                    who will always approach each task with a sense of purpose and attention to detail.
-                    Please do feel free to check out my online profiles below and get in touch using the form.
-                </p>
-            </div>
+                <div className="contact__social__divider">
+                    <div className="contact">
+                        <p className="contact__content">
+                            I’d love to hear about what you’re working on and how I could help. I’m currently
+                            looking for a new role and am open to a wide range of opportunities. My preference
+                            would be to find a position in a company in London. But I’m also happy to hear about
+                            opportunites that don’t fit that description. I’m a hard-working and positive person
+                            who will always approach each task with a sense of purpose and attention to detail.
+                            Please do feel free to check out my online profiles below and get in touch using the form.
+                        </p>
+                    </div>
 
-            <div className="social__contact">
-                <ul className="social__contact__list">
-                    <li className="social__contact__item">
-                        <a href="/">
-                            <Github title="My github profile page"
-                                aria-label="All the projects that i have done on my github page" />
-                        </a>
-                    </li>
-                    <li className="social__contact__item">
-                        <a href="/">
-                            <Twitter title="visit me on twitter"
-                                aria-label="Lets chat on twitter" />
-                        </a>
-                    </li>
-                    <li className="social__contact__item">
-                        <a href="/">
-                            <Linkedin title="linkedin page"
-                                aria-label="Lets chat on linkedin" />
-                        </a>
-                    </li>
-                </ul>
+                    <div className="social__contact">
+                        <ul className="social__contact__list">
+                            <li className="social__contact__item">
+                                <a className="social__link--btn" href="/">
+                                    <Github title="My github profile page"
+                                        aria-label="All the projects that i have done on my github page" />
+                                </a>
+                            </li>
+                            <li className="social__contact__item">
+                                <a className="social__link--btn" href="/">
+                                    <Twitter title="visit me on twitter"
+                                        aria-label="Lets chat on twitter" />
+                                </a>
+                            </li>
+                            <li className="social__contact__item">
+                                <a className="social__link--btn" href="/">
+                                    <Linkedin title="linkedin page"
+                                        aria-label="Lets chat on linkedin" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <form className="form" onSubmit={onSubmit}>
                 <h2 className="form__heading"> Contact Me </h2>
-                <div className="input__container name__container">
-                    <label className="label" htmlFor="name">Name</label>
-                    <input type="text"
-                        className="inputs"
-                        id="fullname"
-                        name="fullName"
-                        placeholder="Chamu mutezva"
-                        value={fullName}
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        required
-                    />
-                    {errors.fullName && <small className="error__alert">{errors.fullName}</small>}
-                </div>
+                <div className="form__input__container">
+                    <div className="input__container name__container">
+                        <label className="label" htmlFor="name">Name</label>
+                        <input type="text"
+                            className="inputs"
+                            id="fullname"
+                            name="fullName"
+                            placeholder="Chamu mutezva"
+                            value={fullName}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            required
+                        />
+                        {errors.fullName && <small className="error__alert">{errors.fullName}</small>}
+                    </div>
 
-                <div className="input__container email__container">
-                    <label className="label" htmlFor="email">Email</label>
-                    <input type="email"
-                        className="inputs"
-                        id="email"
-                        name="email"
-                        placeholder="ckmutezva@gmail.com"
-                        value={email}
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        required
-                    />
-                    {errors.email && <small className="error__alert">{errors.email}</small>}
-                </div>
+                    <div className="input__container email__container">
+                        <label className="label" htmlFor="email">Email</label>
+                        <input type="email"
+                            className="inputs"
+                            id="email"
+                            name="email"
+                            placeholder="ckmutezva@gmail.com"
+                            value={email}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            required
+                        />
+                        {errors.email && <small className="error__alert">{errors.email}</small>}
+                    </div>
 
-                <div className="input__container message__container">
-                    <label className="label" htmlFor="message">Message</label>
-                    <textarea
-                        rows="5"
-                        cols="10"
-                        className="textarea"
-                        id="message"
-                        name="message"
-                        placeholder="message"
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        value={message}>
-                    </textarea>
-                    {errors.message && <small className="error__alert">{errors.message}</small>}
-                </div>
+                    <div className="input__container message__container">
+                        <label className="label" htmlFor="message">Message</label>
+                        <textarea
+                            rows="5"
+                            cols="10"
+                            className="textarea"
+                            id="message"
+                            name="message"
+                            placeholder="message"
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            value={message}>
+                        </textarea>
+                        {errors.message && <small className="error__alert">{errors.message}</small>}
+                    </div>
 
-                <button className="btnSend">Send Message</button>
+                    <button className="btnSend">Send Message</button>
+                </div>
             </form>
 
         </main>
