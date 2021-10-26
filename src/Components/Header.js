05745f1740a4ddca18481e118f0ba237329ebc6d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-10-26 21:13:49 +0200

    logo animation using gsap

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,34 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import gsap from 'gsap'
 import Logo from '../images/logo.svg'
 import Hamburger from '../images/icons/hamburger.svg'
 import Close from '../images/icons/close.svg'
 const Header = () => {
     const [menu, setMenu] = useState(true)
+
     const handleClick = (evt) => {
         const btn = document.querySelector(".show__menu")
         const closeBtn = document.querySelector(".close__menu")
         closeBtn.classList.toggle("hide__btn")
         btn.classList.toggle("hide__btn")
         setMenu(!menu)
+    }
 
+    const handleFocus = () => {
+        gsap.from(".nav__item--btn", { duration: 1.5, opacity: 0, scale: 0.3, ease: "back" })
     }
 
     return (
         <header className="header">
             <div className="logo__container">
-                <Link to="/" className="nav__item--btn logo__item--btn">
-                    <img className="logo" src={Logo} alt="my frontend portfolio" />
+                <Link to="/"
+                    className="nav__item--btn logo__item--btn"
+                    onFocus={handleFocus}>
+
+                    <img className="logo"
+                        src={Logo}
+                        alt="my frontend portfolio" />
                 </Link>
             </div>
             <nav className="nav" aria-label="main navigation">
@@ -53,13 +63,27 @@ const Header = () => {
                 <div className={`modal__container ${menu ? "hide__modal" : ""}`}>
                     <ul className="nav__list__items">
                         <li className="nav__list__item">
-                            <Link to="/" className="nav__item--btn"  onClick={handleClick}>Home</Link>
+                            <Link to="/"
+                                className="nav__item--btn"                               
+                                onClick={handleClick}>
+                                Home
+                            </Link>
                         </li>
+
                         <li className="nav__list__item">
-                            <Link to="/PortfolioPage" className="nav__item--btn"  onClick={handleClick}>Portfolio</Link>
+                            <Link to="/PortfolioPage"
+                                className="nav__item--btn"                               
+                                onClick={handleClick}>
+                                Portfolio
+                            </Link>
                         </li>
+
                         <li className="nav__list__item">
-                            <Link to="/ContactPage" className="nav__item--btn"  onClick={handleClick}>Contact me</Link>
+                            <Link to="/ContactPage"
+                                className="nav__item--btn"                               
+                                onClick={handleClick}>
+                                Contact me
+                            </Link>
                         </li>
                     </ul>
                 </div>
