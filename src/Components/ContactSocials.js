2ef6b9107a7d0f968459387416209f8d4708bad0
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-11-04 06:47:42 +0200

    feat: animation social icons

--- src/Components/ContactSocials.js
+++ src/Components/ContactSocials.js
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { ReactComponent as Github } from "../images/icons/github.svg"
 import { ReactComponent as Twitter } from "../images/icons/twitter.svg"
 import { ReactComponent as Linkedin } from "../images/icons/linkedin.svg"
 const ContactSocials = () => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+
+        gsap.to(".social__icons",
+            {
+                scrollTrigger: {
+                    trigger: ".social__icons",
+                    toggleActions: "restart pause reverse none",
+                },
+                ease: "back",
+                duration: 3,
+                rotation: 360,
+                stagger: 0.5,
+            })
+
+    }, [])
+
     return (
         <div className="social__contact">
             <ul className="social__contact__list">
@@ -11,6 +32,7 @@ const ContactSocials = () => {
                         target="_blank"
                         rel="noreferrer">
                         <Github title="My github profile page"
+                            className="social__icons"
                             aria-label="All the projects that i have done on my github page" />
                     </a>
                 </li>
@@ -20,6 +42,7 @@ const ContactSocials = () => {
                         target="_blank"
                         rel="noreferrer">
                         <Twitter title="visit me on twitter"
+                            className="social__icons"
                             aria-label="Lets chat on twitter" />
                     </a>
                 </li>
@@ -29,6 +52,7 @@ const ContactSocials = () => {
                         target="_blank"
                         rel="noreferrer">
                         <Linkedin title="linkedin page"
+                            className="social__icons"
                             aria-label="Lets chat on linkedin" />
                     </a>
                 </li>
